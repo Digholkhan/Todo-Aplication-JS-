86adbb2ff48aac57f8e2186b4a4fc25ba7bef468
digholkhan <digholkhan@gmail.com>
2025-02-09 15:54:56 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const taskInput = document.getElementById("task-input");
+const taskList = document.getElementById("task-list");
+
+
+function  addTask() {
+    if (taskInput.value === "") {
+      alert("Please enter something!");
+    } 
+    else{
+        let li = document.createElement('li');
+        li.innerHTML = taskInput.value;
+        taskList.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "&#10006";
+        li.appendChild(span)
+      
+    }
+    taskInput.value = "";
+}
+
+taskList.addEventListener("click", function(e){
+    if(e.target.tagName === 'LI'){
+      e.target.classList.toggle('checked')
+    } else if (e.target.tagName === 'SPAN'){
+      e.target.parentElement.remove()
+    }
+},false)
